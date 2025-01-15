167052a830c4f046da327f338533d797a569cd81
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2025-01-15 16:54:55 +0000

    Added router view and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,38 @@
-<!-- frontend/src/App.vue -->
 <template>
-  <div id="app">
-    <NavBar />
+  <div id="app" class="flex flex-col min-h-screen">
+    <NavBar/>
+    <NavCat/>
+
+    <!-- Main Content -->
+    <div class="flex-grow w-full">
+      <router-view/>
+    </div>
+
+    <!-- Footer -->
+    <div class="pr-12">
+      <footer
+          class="border-t-[3px] bg-amber-50 border-black md:flex md:items-center md:justify-between md:p-6 text-black dark:text-white text-sm pr-12">
+      <span class="sm:text-center">© 2025 <a href="#" class="hover:underline">Decorplex Interior Decoration</a>. All Rights Reserved.
+      </span>
+        <ul class="mt-3 flex flex-wrap items-center sm:mt-0">
+          <li>
+            <a target="_blank" href="/privacy" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import NavCat from "@/components/NavCat.vue";
 
 export default {
   name: "App",
   components: {
+    NavCat,
     NavBar,
   },
 };
 </script>
-
-<style>
-/* Add any global styles here */
-</style>
