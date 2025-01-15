73e2c21abf51efc2bd8d7c53876a89422943108a
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2025-01-15 16:52:52 +0000

    Added the store locator page

---
+++ frontend/src/views/StoreLocator.vue
@@ -0,0 +1,55 @@
+<template>
+  <br>
+  <div class="container mx-auto p-8">
+    <b class="text-3xl font-bold">Decorplex Interior Decoration Hamad Town Store</b>
+
+    <br><br><br>
+    <!-- Store Location Section -->
+    <div class="map-container mb-8">
+      <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581.7645590841917!2d50.4884407109919!3d26.13921857702128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49b393f143e6d3%3A0x2e7c52b4d51baa7a!2sDECORPLEX%20INTERIOR%20DECORATION!5e0!3m2!1sen!2suk!4v1736804480305!5m2!1sen!2suk"
+          width="800"
+          height="600"
+          class="w-full h-96 rounded-lg shadow-md"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+
+    <!-- Store Details Section -->
+    <div class="store-details bg-white p-6 rounded-lg shadow-md">
+
+
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">Address:</h3>
+        <p class="text-gray-700">Shop 3973 C Road 372 Block 1203, Hamad Town, Bahrain</p>
+        <p class="text-gray-700">Near Souq Waqif</p>
+      </div>
+
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">Store Timing:</h3>
+        <p class="text-gray-700">7:30 AM to 7:00 PM, Saturday to Thursday</p>
+      </div>
+
+      <div class="mb-4">
+        <h3 class="text-xl font-semibold">Contact Us:</h3>
+        <p class="text-gray-700">+97332295300</p>
+      </div>
+
+      <div>
+        <h3 class="text-xl font-semibold">Email:</h3>
+        <p class="text-gray-700">decorplex.bh@gmail.com</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+}
+</style>
+<script setup>
+</script>
