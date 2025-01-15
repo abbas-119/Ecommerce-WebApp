5de9ce99b93fe805440470583159e7dceed742e2
Abbas Mandasorwala <aam119@student.bham.ac.uk>
2025-01-15 16:53:26 +0000

    Made 2 separate navigation for categories and top

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,121 +1,56 @@
 <template>
-  <div class="bg-amber-50 min-h-screen relative pr-12">
+  <div class="pr-12">
     <div @click="handleClickOutside">
-      <div class="bg-amber-50  py-2 flex justify-between px-4">
+      <!-- Top Contact Section -->
+      <div class="bg-amber-50 py-2 flex justify-between px-4 border-b border-black">
         <div class="flex items-center space-x-4">
           <span>Phone: +973 32295300</span>
-          <a href="#" class="hover:underline">Store Locator
-           <font-awesome-icon :icon="['fas', 'store']" />
+          <a @click="goToStoreLocator" class="hover:underline cursor-pointer">Store Locator
+            <font-awesome-icon :icon="['fas', 'store']"/>
           </a>
         </div>
         <div class="flex items-center space-x-4">
-          <a href="#" class="hover:underline">English</a>
+          <!--          <a href="#" class="hover:underline">English</a>-->
+          <img src="https://flagcdn.com/w40/bh.png" alt="Bahrain Flag" class="h-4 w-6 object-contain"/>
           <a href="#" class="hover:underline">عربي</a>
         </div>
       </div>
-      <div class="navbar flex items-center justify-between bg-white p-12">
-        <div class="logo">
-          <img src="@/assets/Decorplex Logo.png" alt="Decorplex Logo" class="h-21 w-60 object-contain" />
+
+      <!-- Navbar Section -->
+      <div class="navbar flex items-center justify-center bg-white p-12 border-gray-200">
+        <div class="logo mr-40">
+          <img @click="goToHome" src="@/assets/Decorplex Logo.png" alt="Decorplex Logo"
+               class="h-21 w-60 object-contain cursor-pointer"/>
         </div>
-        <div class="flex items-center flex-grow max-w-lg ml-6">
-          <input type="text" placeholder="What are you looking for?" class="search-bar p-1.5 w-full border rounded-l-md" />
-          <button class="bg-gray-200 p-1.5 border rounded-r-md">
-            <font-awesome-icon :icon="['fas', 'search']" />
+        <div class="flex items-center flex-grow max-w-2xl">
+          <input type="text" placeholder="What are you looking for?"
+                 class="search-bar p-1.5 w-full border border-gray-300 rounded-l-md"/>
+          <button class="bg-gray-200 p-1.5 border border-gray-300 rounded-r-Kmd">
+            <font-awesome-icon :icon="['fas', 'search']"/>
           </button>
         </div>
-        <div class="links flex items-center ml-4">
+        <div class="links flex items-center ml-40">
           <a href="#" class="text-yellow-500">Sign Up Now</a>
         </div>
       </div>
 
-      <div class="categories flex justify-around w-full mt-2.5">
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Door Handles</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Door Handles -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Door knobs</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Door knobs -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Door Locks</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Door Locks -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Hinges</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Hinges -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Cabinet Handles</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Cabinet Handles -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <div class="relative group">
-          <div class="flex items-center mx-2.5 text-black hover:bg-gray-100 p-2 rounded-md">
-            <a href="#">Tools</a>
-            <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1" />
-          </div>
-          <!-- Dropdown for Tools -->
-          <div class="absolute hidden group-hover:block bg-white border mt-1 w-48">
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type A</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type B</a>
-            <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
-          </div>
-        </div>
-        <button class="sale-button bg-red-700 text-white border-none p-1.5">Sale</button>
-      </div>
-     <div class="fixed right-0 top-0 h-full w-12 bg-yellow-700 text-white p-2.5 flex flex-col items-center justify-center z-50">
-        <div class="transform -rotate-90 whitespace-nowrap cursor-pointer" @click="toggleBasket">
+
+      <!-- Basket View Button -->
+      <div
+          class="fixed right-0 top-0 h-full w-12 bg-yellow-700 text-white p-2.5 flex flex-col items-center justify-center z-50 border-l border-gray-300 z-50">
+        <div class="transform -rotate-90 whitespace-nowrap cursor-pointer z-50" @click="toggleBasket">
           VIEW BASKET
-          <font-awesome-icon :icon="['fas', 'shopping-cart']" class="text-yellow-500 ml-2.5" />
+          <font-awesome-icon :icon="['fas', 'shopping-cart']" class="text-yellow-500 ml-2.5"/>
         </div>
       </div>
     </div>
 
     <!-- Slider for basket -->
-    <div class="fixed right-0 top-0 h-full w-80 bg-white shadow-lg transform transition-transform duration-300 pr-12"
-         :class="{'translate-x-full': !basketVisible, 'translate-x-0': basketVisible}"
-         @click.stop>
-      <div class="p-4">
+    <div
+        class="fixed right-0 top-0 h-full w-80 bg-white shadow-lg transform transition-transform duration-300 pr-12 border-l border-gray-200 z-50"
+        :class="{'translate-x-full': !basketVisible, 'translate-x-0': basketVisible}"
+        @click.stop>
+      <div class="p-4 border-b border-gray-200">
         <div class="flex justify-between items-center">
           <h2 class="text-2xl font-bold mb-4">Your Basket</h2>
           <button @click="toggleBasket" class="text-red-500">Close</button>
@@ -143,6 +78,12 @@ export default {
       if (this.basketVisible && !this.$el.querySelector('.fixed.right-0').contains(event.target)) {
         this.basketVisible = false;
       }
+    },
+    goToStoreLocator() {
+      this.$router.push('/store-locator');
+    },
+    goToHome() {
+      this.$router.push('/');
     }
   },
   mounted() {
@@ -158,6 +99,7 @@ export default {
 .translate-x-full {
   transform: translateX(100%);
 }
+
 .translate-x-0 {
   transform: translateX(0);
 }
---
+++ frontend/src/components/NavCat.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="categories flex justify-center w-full bg-white border-b border-gray-300 pb-2 sticky top-0 z-30">
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Door Handles</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Door Handles -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Door Knobs</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Door Knobs -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Door Locks</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Door Locks -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Hinges</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Hinges -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Cabinet Handles</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Cabinet Handles -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+
+    <div class="relative group z-50">
+      <div class="flex items-center mx-5 text-black hover:bg-gray-100 p-2 border-gray-200">
+        <a href="#">Tools</a>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" class="ml-1"/>
+      </div>
+      <!-- Dropdown for Tools -->
+      <div class="absolute hidden group-hover:block bg-white border mt-0.5 w-48 border-gray-200">
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type A</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100 border-b border-gray-200">Type B</a>
+        <a href="#" class="block px-4 py-2 text-black hover:bg-gray-100">Type C</a>
+      </div>
+    </div>
+    <button class="sale-button mx-5 text-red-700 font-bold border-none p-1.5 border border-gray-200">Sale</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+<script setup>
+</script>
